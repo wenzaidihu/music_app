e2d268662b8350f58a1b2930b866de83661bd8fb
wenzaidihu <750625920@qq.com>
2019-11-20 15:10:47 +0800

    初始化项目，安装依赖，配置别名

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app" @touchmove.prevent>
+    kong
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
