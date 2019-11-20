19d608a4aedc9aa4e2f407ece39dd287eef18678
wenzaidihu <750625920@qq.com>
2019-11-20 17:13:30 +0800

    header组件编写

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
-  <div id="app" @touchmove.prevent>
-    kong
+  <div id="app">
+    <MHheader></MHheader>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import MHheader from 'components/m-header/m-header'
 
+  export default {
+    components: {
+      MHheader
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  #app
+    color: $color-theme
 </style>
