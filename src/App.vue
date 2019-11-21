ccf8cf297b2038d2e53704362568cce1184f9682
wenzaidihu <750625920@qq.com>
2019-11-21 23:35:03 +0800

    配置路由，编写tab组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
     <MHheader></MHheader>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import MHheader from 'components/m-header/m-header'
+  import Tab from 'components/tab/tab'
 
   export default {
     components: {
-      MHheader
+      MHheader,
+      Tab
     }
   }
 </script>
---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    推荐页面
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: "Recommend"
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
